627b8cab1cb35b6ce2847d74fba6125bb95b0156
sailokeshreddy-ai <reddywarisailokesh037@gmail.com>
2026-08-31 15:37:49 +0530

    Indexing

---
+++ strings_conditionals/Indexing.py
@@ -0,0 +1,8 @@
+# Indexing in strings
+
+str = "Python"
+ch = str[0] #ch is assigned the first character of the string
+
+#str[4] = "a" #we cannot change the value of a character in a string because strings are immutable in Python
+#print(str) # This will cause a TypeError because strings in Python are immutable
+print(ch) # This will print 'P', the first character of the string
