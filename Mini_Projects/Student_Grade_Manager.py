1cabc5c37aab8600cd78842f32702424c472ff4d
sailokeshreddy-ai <reddywarisailokesh037@gmail.com>
2026-08-31 13:35:44 +0530

    Folder Name changed

---
+++ Mini_Projects/Student_Grade_Manager.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+maths = int(input("Enter your Maths marks: "))
+python = int(input("Enter your Python marks: "))
+english = int(input("Enter your English marks: "))
+
+total = maths + python + english
+
+percentage = (total / 300) * 100
