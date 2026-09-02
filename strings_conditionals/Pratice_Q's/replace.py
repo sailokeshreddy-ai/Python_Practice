262ffcc9b27f5734a9984da1e163206a1bcb00f9
sailokeshreddy-ai <reddywarisailokesh037@gmail.com>
2026-09-02 12:25:03 +0530

    Replacing string

---
+++ strings_conditionals/Pratice_Q's/replace.py
@@ -0,0 +1,4 @@
+# Replacing the old string with new string
+str = "I am a student of AI Engineering"
+new_str = str.replace("Engineering", "Engineering college")
+print(new_str)  # Output: I am a student of AI Engineering college
