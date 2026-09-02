17c1dc459dd83ef260d018230be1cef6511eda5c
sailokeshreddy-ai <reddywarisailokesh037@gmail.com>
2026-09-02 12:16:57 +0530

    Functions of strings

---
+++ strings_conditionals/string_functions.py
@@ -0,0 +1,12 @@
+# String Functions
+str = "Ai Engineering"
+print(str.capitalize())  # Output: AI ENGINEERING
+print(str.upper())  # Output: AI ENGINEERING
+print(str.lower())  # Output: ai engineering
+print(str.title())  # Output: Ai Engineering
+print(str.endswith("g"))  # Output: True
+print(str.endswith("A"))  # Output: False
+print(str.replace("Engineering", "Engineer"))  # Output: Ai Engineer
+print(str.find("Engineering"))  # Output: 3
+print(str.find("Engineer"))  # Output: 3
+print(str.count("e"))  # Output: 2
