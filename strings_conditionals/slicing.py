9aebd8da4bf147a2aedffd722e01d17bd115b04c
sailokeshreddy-ai <reddywarisailokesh037@gmail.com>
2026-09-02 12:08:36 +0530

    Added slicing

---
+++ strings_conditionals/slicing.py
@@ -0,0 +1,8 @@
+# Slicing in strings
+str = "Ai Engineering"
+print(str[0:2])  # Output: Ai
+print(str[3:14])  # Output: Engineering
+
+#Negative indexing
+str1 = "Loki Reddy"
+print(str1[-5:-2]) # Output: Red
